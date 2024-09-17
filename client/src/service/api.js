57fdbe38da23a2afa359520fb0e07a13cafe1a3f
Sonu Kumar <50261418+sonukarn@users.noreply.github.com>
2024-09-17 16:43:20 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from "../constants/config";
 import { getAccessToken, getType } from "../utils/common-utils";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://sonuwebtechblog.onrender.com";
 const axiosInstance = axios.create({
   baseURL: API_URL,
   timeout: 20000,
